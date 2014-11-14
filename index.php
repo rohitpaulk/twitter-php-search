93c1f107fa164e0ba10148eb2b304d1e30fdc695
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-11-14 21:22:43 +0530

    Greetings program

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Twitter API Client</title>
+		<link rel="stylesheet" src="/assets/main.css" />
+	</head>
+	<body>
+		<?php
+			echo "Greetings, program!";
+		?>
+	</body>
+</html>
+
