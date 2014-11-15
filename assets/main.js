60d9b1e70ab0d1755c7ba962639aa4126f9ace4a
Rohit Paul Kuruvilla <rohitpaul@iitrpr.ac.in>
2014-11-15 14:35:19 +0530

    Use widgets-js to show embedded tweets.

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,11 @@
 $(document).ready(function () {
 	$.get('get_tweets.php', function(data) {
-		console.log(data);
-
-		html = "";
-		var keys = [];
-
-		$.each(data, function(key, value){
-			$.get("https://api.twitter.com/1/statuses/oembed.json?id=" + key, function (data) {
-				$('#tweetbox').append(data.html);
+		twttr.ready(function(twttr) {
+			$.each(data, function(key, value){
+				$('#tweetbox').append("<div id='" + key + "'></div>");
+				twttr.widgets.createTweet(key, document.getElementById(key), {});
 			});
-			html+= key + " " + value + "<br>";
 		});
-
-
-
-		//$('#tweetbox').html(html);
 	});
-	console.log(twttr);
-	//twttr.widgets.createTweet();
 });
+
